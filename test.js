e14f30938d3560c194a5b9084a752e549696d586
antonia-k <a_kellerwessel@hotmail.de>
2019-04-30 21:16:58 +0200

    odified test

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log('hello! It is working');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "a1i2n3o4t5n6a"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
